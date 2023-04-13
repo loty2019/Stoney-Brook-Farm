9493ed963fa9092ae6e65f585bd7260b0dccbcb5
loty2019 <lorenzo.orio@icloud.com>
2023-04-12 22:15:02 -0400

    Working Log In night

--- js/navScript.js
+++ js/navScript.js
@@ -1,7 +1,22 @@
 const hamburger = document.querySelector(".hamburger");
 const mainNav = document.querySelector(".main-nav");
+const navLinks = document.querySelectorAll(".main-nav li");
 
+function animateLinks(links, animationDirection) {
+  links.forEach((link, index) => {
+    setTimeout(() => {
+      link.style.transform = animationDirection === "in" ? "translateY(0)" : "translateY(-100%)";
+      link.style.opacity = animationDirection === "in" ? "1" : "0";
+    }, index * 100);
+  });
+}
 
 hamburger.addEventListener("click", () => {
   mainNav.classList.toggle("show-nav");
+
+  if (mainNav.classList.contains("show-nav")) {
+    animateLinks(navLinks, "in");
+  } else {
+    animateLinks(navLinks, "out");
+  }
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger");
   const mainNav = document.querySelector(".main-nav");
+  const navLinks = document.querySelectorAll(".main-nav li");
+  const hamburgerLines = document.querySelectorAll(".line");
 
+  function animateLinks(links, animationDirection, callback) {
+    const delay = animationDirection === "in" ? 100 : -100;
+    const baseDelay =
+      animationDirection === "in" ? 0 : (links.length - 1) * 100;
 
+    links.forEach((link, index) => {
+      setTimeout(() => {
+        link.style.transform =
+          animationDirection === "in" ? "translateY(0)" : "translateY(-100%)";
+        link.style.opacity = animationDirection === "in" ? "1" : "0";
+
+        if (callback && index === links.length - 1) {
+          setTimeout(callback, Math.abs(delay));
+        }
+      }, baseDelay + index * delay);
+    });
+  }
+
+  function toggleHamburger() {
+    hamburger.classList.toggle("active");
+  }
+  
   hamburger.addEventListener("click", () => {
-    mainNav.classList.toggle("show-nav");
+    if (mainNav.classList.contains("show-nav")) {
+      animateLinks(navLinks, "out", () => {
+        setTimeout(() => {
+          mainNav.classList.remove("show-nav");
+          toggleHamburger();
+        }, 600);
+      });
+    } else {
+      mainNav.classList.add("show-nav");
+      animateLinks(navLinks, "in");
+      toggleHamburger();
+    }
   });
 
   // Background change
@@ -31,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     backImage.classList.remove("fadeOut");
     backImage.classList.add("fadeIn");
-    
+
     setTimeout(() => {
       backImage.classList.remove("fadeIn");
       backImage.classList.add("fadeOut");
