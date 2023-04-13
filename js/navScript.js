c4a469c91a9cdbfe080c8d67e5bf3068c2ee350d
loty2019 <lorenzo.orio@icloud.com>
2023-04-12 22:35:14 -0400

    fix fix idk

--- js/navScript.js
+++ js/navScript.js
@@ -1,22 +1,41 @@
 const hamburger = document.querySelector(".hamburger");
-const mainNav = document.querySelector(".main-nav");
-const navLinks = document.querySelectorAll(".main-nav li");
+  const mainNav = document.querySelector(".main-nav");
+  const navLinks = document.querySelectorAll(".main-nav li");
+  const hamburgerLines = document.querySelectorAll(".line");
 
-function animateLinks(links, animationDirection) {
-  links.forEach((link, index) => {
-    setTimeout(() => {
-      link.style.transform = animationDirection === "in" ? "translateY(0)" : "translateY(-100%)";
-      link.style.opacity = animationDirection === "in" ? "1" : "0";
-    }, index * 100);
-  });
-}
+  function animateLinks(links, animationDirection, callback) {
+    const delay = animationDirection === "in" ? 100 : -100;
+    const baseDelay =
+      animationDirection === "in" ? 0 : (links.length - 1) * 100;
 
-hamburger.addEventListener("click", () => {
-  mainNav.classList.toggle("show-nav");
+    links.forEach((link, index) => {
+      setTimeout(() => {
+        link.style.transform =
+          animationDirection === "in" ? "translateY(0)" : "translateY(-100%)";
+        link.style.opacity = animationDirection === "in" ? "1" : "0";
 
-  if (mainNav.classList.contains("show-nav")) {
-    animateLinks(navLinks, "in");
-  } else {
-    animateLinks(navLinks, "out");
+        if (callback && index === links.length - 1) {
+          setTimeout(callback, Math.abs(delay));
+        }
+      }, baseDelay + index * delay);
+    });
   }
-});
+
+  function toggleHamburger() {
+    hamburger.classList.toggle("active");
+  }
+  
+  hamburger.addEventListener("click", () => {
+    if (mainNav.classList.contains("show-nav")) {
+      animateLinks(navLinks, "out", () => {
+        setTimeout(() => {
+          mainNav.classList.remove("show-nav");
+          toggleHamburger();
+        }, 600);
+      });
+    } else {
+      mainNav.classList.add("show-nav");
+      animateLinks(navLinks, "in");
+      toggleHamburger();
+    }
+  });
